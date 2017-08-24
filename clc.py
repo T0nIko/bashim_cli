fa67f7491b07f129e4e6c2f0ebcb526222fd24c3
Anton Kostryukov <anton97kos@gmail.com>
2017-08-24 21:13:31 +0300

    fixed something but still a lot to be done

--- clc.py
+++ clc.py
@@ -4,8 +4,16 @@ import core
 
 
 @click.command()
-@click.argument('mode')
-def bashim_clc(mode=''):
-    for q in core.get_quotes(core.URLS[mode]):
-        click.echo(q)
-        click.echo('-------------------------------')
+@click.argument('mode', required=False, default='')
+@click.argument('amount', required=False, default=5)
+def bashim_clc(mode, amount):
+    while True:
+        try:
+            quotes = core.get_quotes(core.URLS[mode], amount)
+        except:
+            quotes = core.get_quotes(core.ALIASES[mode], amount)
+
+        core.pprint(quotes)
+
+        if click.confirm('Another set of quotes?', abort=True):
+            pass
--- core.py
+++ core.py
@@ -1,4 +1,6 @@
 import re
+
+import click
 import requests
 from bs4 import BeautifulSoup as BS
 
@@ -9,17 +11,36 @@ ABYSS_URL = 'http://bash.im/abyss'
 ABYSS_TOP_URL = 'http://bash.im/abysstop'
 ABYSS_BEST_URL = 'http://bash.im/abyssbest'
 
-URLS = {'rnd': RND_URL,
-        'best': BEST_URL,
-        'new': NEWEST_URL,
-        'abyss': ABYSS_URL,
-        'abyss_top': ABYSS_TOP_URL,
-        'abyss_best': ABYSS_BEST_URL}
+URLS = {
+    'random': RND_URL,
+    'best': BEST_URL,
+    'new': NEWEST_URL,
+    'abyss': ABYSS_URL,
+    'abyss_top': ABYSS_TOP_URL,
+    'abyss_best': ABYSS_BEST_URL,
+}
+
+ALIASES = {
+    '': URLS['random'],
+    'rnd': URLS['random'],
+    'r': URLS['random'],
+    'b': URLS['best'],
+    'n': URLS['new'],
+    'a': URLS['abyss'],
+    'at': URLS['abyss_top'],
+    'ab': URLS['abyss_best'],
+}
 
 
-def get_quotes(url):
+def get_quotes(url, amount):
     soup = BS(requests.get(url).content, 'lxml')
 
     quotes_raw = soup('div', class_='text')
 
-    return [re.sub(r'(<br\/>)', '\n', str(q)[18:-6]) for q in quotes_raw]
+    return [re.sub(r'(&gt;)*|(&lt;)*', '', re.sub(r'(<br\/>)', '\n', str(q)[18:-6])) for q in quotes_raw][0:amount]
+
+
+def pprint(quotes):
+    for q in quotes:
+        click.echo(q)
+        click.echo('-------------------------------')
